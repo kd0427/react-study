315a3fdcbaab1cf45f3cba32da853a617fc07a2c
kd0427 <rlaemek@naver.com>
2025-03-25 23:52:03 +0900

    day2

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,20 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const state = useState();
+  const [state, setState] = useState(0);
   console.log(state);
-  return <></>;
+  return (
+    <>
+      <h1>{state}</h1>
+      <button
+        onClick={() => {
+          setState(state + 1);
+        }}
+      >
+        증가
+      </button>
+    </>
+  );
 }
 
 export default App;
